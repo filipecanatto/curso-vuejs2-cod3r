f630ee35c5442f474f33e410563702982350d6b8
wcanatto <wcanatto@cpqd.com.br>
2019-05-07 16:22:21 -0300

    c101

---
+++ topics/conditional_rendering_and_lists/challenge-06-conditional_rendering_and_lists/myslef/app.js
@@ -0,0 +1,28 @@
+
+new Vue({
+	el: '#desafio',
+	data: {
+		toggle: true,
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		arrayLivros: [
+			{
+				titulo: 'O Senhor dos Anéis',
+				autor: 'J.R.R. Tolkiens',
+				volume: '3'
+			},
+			{
+				titulo: 'As Cronicas de Narnia',
+				autor: 'C.S Lewis',
+				volume: '2'
+			}
+		]
+		,
+		aluno: [{
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		}]
+	}
+});
+
+
