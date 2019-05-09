22fec9638ef29eefc27a055445864646948b1cc0
wcanatto <wcanatto@cpqd.com.br>
2019-05-09 15:21:54 -0300

    ci01

--- topics/conditional_rendering_and_lists/final_challenge/app.js
+++ topics/conditional_rendering_and_lists/final_challenge/app.js
@@ -1,15 +1,19 @@
 new Vue({
 	el: '#app',
 	data: {
-		playerWidhtInPerCent: '50%',
 		playerWidht: 50
 
 	},
+	watch: {
+		playerWidht(newV, oldV) {
+			console.log('entrei');
+
+		}
+	},
 	methods: {
 		attack() {
-			this.playerWidht -= 10;
-			this.playerWidhtInPerCent = this.playerWidht + '%';
-			console.log(this.playerWidhtInPerCent);
+			this.playerWidht -= 5;
+			console.log(this.playerWidht);
 		}
 	}
 });
