7106abcf5c2ad83d26f5f7e63f0855c56e33631e
wcanatto <wcanatto@cpqd.com.br>
2019-05-10 16:17:36 -0300

    ci01

--- topics/conditional_rendering_and_lists/final_challenge/app.js
+++ topics/conditional_rendering_and_lists/final_challenge/app.js
@@ -2,20 +2,31 @@ new Vue({
 	el: '#app',
 	data: {
 		playerLife: 100,
+		monsterLife: 100,
+		newGame: true,
 		messages: [
-			
+
 		]
 
 
 	},
 	watch: {
 		playerLife(newV, oldV) {
+			if (newV < oldV){
+				return;
+			}
 			console.log('entrei');
 			this.messages.push('The player hint the enemy with 10% of damage.');
 
 		}
 	},
 	methods: {
+		beginFromZero() {
+			this.newGame = !this.newGame
+			this.playerLife = 100;
+			this.monsterLife = 100;
+			this.messages = [''];
+		},
 		attack() {
 			this.playerLife -= 5;
 			console.log(this.playerLife);
