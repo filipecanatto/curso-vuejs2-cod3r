adda27adbaa6b4f6fd439100e89cfee92866d487
wcanatto <wcanatto@cpqd.com.br>
2019-05-13 09:12:38 -0300

    ci01

--- topics/conditional_rendering_and_lists/final_challenge/app.js
+++ topics/conditional_rendering_and_lists/final_challenge/app.js
@@ -4,6 +4,8 @@ new Vue({
 		playerLife: 100,
 		monsterLife: 100,
 		newGame: true,
+		showResult: false,
+		playerDied: false,
 		messages: [
 
 		]
@@ -16,11 +18,21 @@ new Vue({
 			if (newV <= 20) {
 				this.playerLifeState = 'red';
 			}
+			if (newV <= 0) {
+				this.showResult = true
+				this.playerDied = true
+				this.messages = ['']
+				return
+			}
 			this.messages.push('The player hint the enemy with ' + (oldV - newV) + ' of damage.');
 		},
 		monsterLife(newV, oldV) {
 			if (newV > oldV) {
 				return;
+			}
+
+			if (this.playerDied) {
+				return
 			}
 
 			if (newV <= 20) {
@@ -35,12 +47,15 @@ new Vue({
 			this.playerLife = 100;
 			this.monsterLife = 100;
 			this.messages = [''];
+			this.playerDied = false
+			this.showResult = false
+			this.newGame = false
 		},
 		attack() {
-			this.playerLife -= 9
-			this.monsterLife -= 6
-			this.playerLife = (this.playerLife < 0) ? 0 : this.playerLife;
-			this.monsterLife = (this.monsterLife < 0) ? 0 : this.monsterLife;
+			this.playerLife -= Math.floor((Math.random() * 9) + 1)
+			this.monsterLife -= Math.floor((Math.random() * 6) + 1)
+			this.playerLife = (this.playerLife < 0) ? 0 : this.playerLife
+			this.monsterLife = (this.monsterLife < 0) ? 0 : this.monsterLife
 		},
 	}
 });
