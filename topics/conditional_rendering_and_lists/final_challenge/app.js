a6d34ad1c03faa8fcdc1f1230deb4e4726ce8fcd
wcanatto <wcanatto@cpqd.com.br>
2019-05-13 13:48:02 -0300

    ci01

--- topics/conditional_rendering_and_lists/final_challenge/app.js
+++ topics/conditional_rendering_and_lists/final_challenge/app.js
@@ -3,72 +3,72 @@ new Vue({
 	data: {
 		playerLife: 100,
 		monsterLife: 100,
-		newGame: true,
+		showMenu: true,
 		showResult: false,
-		playerDied: false,
+		isPlayerDied: false,
 		messages: [
 
 		]
 	},
 	watch: {
 		playerLife(newV, oldV) {
-			if (newV > oldV) {
-				return;
-			}
-			if (newV <= 20) {
-				this.playerLifeState = 'red';
-			}
-			if (newV <= 0) {
-				this.showResult = true
-				this.playerDied = true
-				this.messages = ['']
-				return
-			}
+			console.log('playerLife')
+			this.alertLowLife(this.playerLife)
 			this.messages.push('The player hint the enemy with ' + (oldV - newV) + ' of damage.');
 		},
 		monsterLife(newV, oldV) {
-			if (newV > oldV) {
-				return;
-			}
-
-			if (newV <= 0) {
-				this.showResult = true
-				this.playerDied = false
-				this.messages = ['']
-				return
-			}
-
-			if (this.playerDied) {
-				return
-			}
-
-			if (newV <= 20) {
-				this.monsterLifeState = 'red';
-			}
+			this.alertLowLife(this.playerLife)
 			this.messages.push('The monster hint the enemy with ' + (oldV - newV) + ' of damage.');
 		}
 	},
 	methods: {
-		beginFromZero() {
-			this.newGame = !this.newGame
-			this.playerLife = 100;
-			this.monsterLife = 100;
-			this.messages = [''];
-			this.playerDied = false
+		restart() {
+			this.restartLifes()
+			this.isPlayerDied = false
 			this.showResult = false
-			this.newGame = false
+			this.showMenu = false
+		},
+		showResultInScreen(value) {
+			this.messages = [''];
+			this.isPlayerDied = value
+			this.showResult = true
 		},
 		attack() {
-			this.playerLife -= Math.floor((Math.random() * 9) + 1)
-			this.monsterLife -= Math.floor((Math.random() * 6) + 1)
-			this.playerLife = (this.playerLife < 0) ? 0 : this.playerLife
-			this.monsterLife = (this.monsterLife < 0) ? 0 : this.monsterLife
+			console.log('attack')
+			let playerDamage = Math.floor((Math.random() * 9) + 1)
+			let monsterDamage = Math.floor((Math.random() * 6) + 1)
+			if ((this.playerLife - playerDamage) <= 0) {
+				this.playerLife = 0;
+				return this.showResultInScreen(true)
+			} else {
+				this.playerLife -= playerDamage;
+			}
+
+			if ((this.monsterLife - monsterDamage) <= 0) {
+				this.monsterLife = 0;
+				return this.showResultInScreen(true)
+			} else {
+				this.monsterLife -= monsterDamage;
+			}
 		},
 		specialAttack() {
+			if (this.playerLife == 0 || this.monsterLife == 0) {
+				return
+			}
 			this.playerLife -= Math.floor((Math.random() * 6) + 1)
 			this.monsterLife -= Math.floor((Math.random() * 9) + 1)
-			this.playerLife = (this.playerLife < 0) ? 0 : this.playerLife
-			this.monsterLife = (this.monsterLife < 0) ? 0 : this.monsterLife
 		},
+		alertLowLife(element) {
+			if (element <= 20) {
+				element = 'red';
+			}
+		},
+		restartLifes() {
+			this.heal(this.playerLife, 100);
+			this.heal(this.monsterLife, 100);
+		},
+		heal(element, value) {
+			this.element = value;
+		}
 	}
 });
