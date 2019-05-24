8d9e90f261261b802150182382f8d95152bac1b4
Wilton Filipe Canatto <wcanatto@aquarius.cpqd.com.br>
2019-05-24 13:05:37 -0300

    curso vue cod3r

---
+++ topics/components/intro-components/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app">
+        <h1>Counters</h1>
+        <app-counter></app-counter>
+        <app-counter/>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
---
+++ topics/components/intro-components/src/Counter.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="counter">
+        <span>{{count}}</span>
+        <button @click='add'>+</button>
+        <button @click='subtract'>-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+
+    },
+    methods: {
+        add() {
+            this.count++
+        },
+        subtract() {
+            this.count--
+        }
+    }
+}
+</script>
+
+<style>
+.counter span {
+    border-bottom: 1px solid #CCC;
+    height: 30px;
+    padding: 5px 25px;
+}
+
+.counter button {
+    height: 30px;
+    width: 30x;
+    border-radius: 15px;
+    background-color: coral;
+    color: #fff;
+    margin-left: 10px;
+    outline: none;
+}
+</style>
