f61a9a26389550bf28da39a0c1676fed86a9df00
Wilton Filipe Canatto <wcanatto@aquarius.cpqd.com.br>
2019-05-28 08:12:43 -0300

    ci01

---
+++ topics/components/challenge-07-components/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-app>
+		<app-navigation/>
+		<app-title/>
+		<app-carousel/>
+	</v-app>
+</template>
+
+<script>
+import Navigation from './Navigation.vue'
+import Title from './Title.vue'
+import Carousel from './Carousel.vue'
+import Footer from './Footer.vue'
+
+export default {
+
+	components: {
+		'app-navigation': Navigation,
+		'app-title': Title,
+		'app-carousel': Carousel,
+		'app-footer': Footer
+	},
+
+
+}
+</script>
---
+++ topics/components/challenge-07-components/src/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase">
+            <span>Component</span>
+            <span class="font-weight-light">Challenge</span>
+        </v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
