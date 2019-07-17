65ee296d3308c3f9fed534c7f20dfb5233c51741
Filipe Canatto <filipecanatto@gmail.com>
2019-07-17 20:41:35 -0300

    passando-arg-para-diretiva-personalizada

--- topics/entendendo-e-usando-diretivas/diretivas-exercicios/src/App.vue
+++ topics/entendendo-e-usando-diretivas/diretivas-exercicios/src/App.vue
@@ -4,12 +4,21 @@
 		<p v-text="'usando a diretiva v-text'">tst</p>
 		<p v-html="'usando a diretiva <strong>v-html</strong>'"></p>
 		<hr>
+		<p v-destaque="cor">Usando diretiva personalizada</p>
 		<p v-destaque="'lightblue'">Usando diretiva personalizada</p>
+		<p v-destaque:fundo="'lightblue'">Usando diretiva personalizada</p>
+		<!-- diretiva:arg.modificadores=valor-->
+		<p v-teste:arg.mod1.mod2="'valie'"></p>
 	</div>
 </template>
 
 <script>
 export default {
+	data(){
+		return{
+			cor:'lightblue'
+		}
+	}
 	
 }
 </script>
