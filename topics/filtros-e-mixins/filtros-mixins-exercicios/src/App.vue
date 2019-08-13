5643472230d81a149844cd197cb4e656d07c9a16
Filipe Canatto <filipecanatto@gmail.com>
2019-08-12 21:28:36 -0300

    duplicando-codigo-para-usar-mixins

--- topics/filtros-e-mixins/filtros-mixins-exercicios/src/App.vue
+++ topics/filtros-e-mixins/filtros-mixins-exercicios/src/App.vue
@@ -4,13 +4,26 @@
 		<hr>
 		<p>{{cpf | cpfFilter | inverter}}</p>
 		<input :value='cpf | cpfFilter' type="text">
+		<hr>
+		<Frutas></Frutas>
+		<hr>
+		<div>
+			<ul>
+				<li v-for='fruta in this.frutas' :key='fruta'>{{fruta}}</li>
+			</ul>
+			<input v-model='fruta' @keydown.enter="add" type="text">
+		</div>
 	</div>
 </template>
 
 <script>
+import Frutas from './Frutas'
 export default {
+	components: {
+		Frutas
+	},
 	filters: {
-		cpfFilter(value){
+		cpfFilter(value) {
 			// transforma string em vetor de char
 			const arr = value.split('')
 			arr.splice(3, 0, '.')
@@ -22,7 +35,14 @@ export default {
 	},
 	data() {
 		return {
-			cpf: '35895712588'
+			cpf: '35895712588',
+			fruta: '',
+			frutas: ['banana', 'laranja', 'maça']
+		}
+	},
+	methods: {
+		add() {
+			this.frutas.push(this.fruta)
 		}
 	}
 }
@@ -39,9 +59,7 @@ export default {
 	font-size: 2.5rem;
 }
 
-input{
+input {
 	font-size: 2.5rem;
 }
-
-
 </style>
---
+++ topics/filtros-e-mixins/filtros-mixins-exercicios/src/Frutas.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <ul>
+            <li v-for='fruta in this.frutas' :key='fruta'>{{fruta}}</li>
+        </ul>
+        <input v-model='fruta' @keydown.enter="add" type="text">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            fruta: '',
+            frutas: ['banana', 'laranja', 'maça']
+        }
+    },
+    methods: {
+        add() {
+            this.frutas.push(this.fruta)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
