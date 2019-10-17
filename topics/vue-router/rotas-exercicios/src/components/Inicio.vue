ceb5de09f9cdbdce7075cca255e36de255e985db
Filipe Canatto <filipecanatto@gmail.com>
2019-10-17 16:32:48 -0300

    aula 2

---
+++ topics/vue-router/rotas-exercicios/src/components/Inicio.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="inicio">
+        <h2>Inicio</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ topics/vue-router/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="usuario">
+        <h2>Usuário</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+
+</style>
