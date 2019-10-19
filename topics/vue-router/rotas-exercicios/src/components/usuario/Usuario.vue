ce43c8d37e1e5a68a00a6ddef3eaf6911bf388c5
Filipe Canatto <filipecanatto@gmail.com>
2019-10-19 10:06:15 -0300

    lendo e usando parametros de rotas

--- topics/vue-router/rotas-exercicios/src/components/usuario/Usuario.vue
+++ topics/vue-router/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,14 +2,20 @@
     <div class="usuario">
         <h2>Usuário</h2>
         <hr>
+        <p><strong>Código: {{id}}</strong></p>
         <button sucesso v-on:click='irParaInicio'>Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
-    methods:{
-        irParaInicio(){
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
+    methods: {
+        irParaInicio() {
             this.$router.push('/')
             // or this.$router.push({ path: '/' })
         }
