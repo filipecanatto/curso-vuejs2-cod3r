984c5788c6f8673ffb6b6877500be86f6a8a5da9
Filipe Canatto <filipecanatto@gmail.com>
2019-10-24 16:36:07 -0300

    trabalhando com multiplos vue-routers

--- topics/vue-router/rotas-exercicios/src/router.js
+++ topics/vue-router/rotas-exercicios/src/router.js
@@ -5,6 +5,8 @@ import Usuario from './components/usuario/Usuario'
 import UsuarioLista from './components/usuario/UsuarioLista'
 import UsuarioDetalhe from './components/usuario/UsuarioDetalhe'
 import UsuarioEditar from './components/usuario/UsuarioEditar'
+import Menu from './components/template/Menu'
+import MenuAlt from './components/template/MenuAlt'
 
 Vue.use(Router)
 
@@ -15,10 +17,19 @@ export default new Router(
             {
                 path: '/',
                 name: 'inicio',
-                component: Inicio
+                //component: Inicio
+                components:{
+                    default:Inicio,
+                    menu: Menu
+                }
             }, {
                 path: '/usuario/',
-                component: Usuario,
+                //component: Usuario,
+                components:{
+                    default:Usuario,
+                    menu:MenuAlt,
+                    menuInferior:MenuAlt
+                },
                 props: true,
                 children: [{
                     path: '', component: UsuarioLista
