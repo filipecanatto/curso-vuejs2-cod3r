7dcfc056748e1372b1481e6631f4828a84cd7276
Filipe Canatto <filipecanatto@gmail.com>
2019-10-25 14:44:29 -0300

    controlando o comportamento de rolagem

--- topics/vue-router/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ topics/vue-router/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -4,6 +4,9 @@
         <p><strong>Código: {{id}} / {{ $route.params.id }}</strong></p>
         <p><strong>Completo: </strong> {{ $route.query.completo ? 'Sim' : 'Não' }} </p>
         <p> <strong>Linha :</strong> {{$route.query.lingua}} </p>
+        <div id="rodape">
+            <h3>Curso Vue</h3>
+        </div>
     </div>
 </template>
 
@@ -16,5 +19,7 @@ export default {
 
 
 <style>
-
+#rodape{
+    margin-top: 2000px;
+}
 </style>
--- topics/vue-router/rotas-exercicios/src/router.js
+++ topics/vue-router/rotas-exercicios/src/router.js
@@ -12,7 +12,16 @@ Vue.use(Router)
 
 export default new Router(
     {
-        mode: 'hash',
+        mode: 'history',
+        scrollBehavior(to, from, savedPosition){
+            if (savedPosition){
+                return savedPosition
+            }else if (to.hash){
+                return {selector: to.hash}
+            }else{
+                return {x:0, y:0}
+            }
+        },
         routes: [
             {
                 path: '/',
